8987a88c81af07114f9252a9c693c6f1f0114523
Calvin Gabriel <gabric@devnull.ip-soft.net>
2014-02-28 16:06:03 -0500

    .

--- conversemixer.js
+++ conversemixer.js
@@ -2,4 +2,4 @@ function initEnvironment(){
 }
 $('head').append('<link rel="stylesheet" type="text/css" media="screen" href="https://conversejs.org/converse.min.css">');
 $('head').append('<script src="https://conversejs.org/builds/converse.min.js"></script>');
-$('body').append('<script> require(['converse'], function (converse) { converse.initialize({ allow_otr: true, auto_list_rooms: false, auto_subscribe: false, bosh_service_url: "https://bind.opkode.im", debug: true , hide_muc_server: false, i18n: locales['en'], // Refer to ./locale/locales.js to see which locales are supported prebind: false, show_controlbox_by_default: true, xhr_user_search: false, }); }); </script>');
+$('body').append('<script> require(['converse'], function (converse) { converse.initialize({ allow_otr: true, auto_list_rooms: false, auto_subscribe: false, bosh_service_url: "https://bind.opkode.im", debug: true , hide_muc_server: false, i18n: locales['en'], prebind: false, show_controlbox_by_default: true, xhr_user_search: false, }); }); </script>');
