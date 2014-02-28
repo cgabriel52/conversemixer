45f5357119a37ff16a009b1dd1bd1b009fa76522
Calvin Gabriel <gabric@devnull.ip-soft.net>
2014-02-28 16:22:12 -0500

    .

--- conversemixer.js
+++ conversemixer.js
@@ -2,23 +2,21 @@ function initEnvironment(){
 }
 $('head').append('<link rel="stylesheet" type="text/css" media="screen" href="https://conversejs.org/converse.min.css">');
 $('head').append('<script src="https://conversejs.org/builds/converse.min.js"></script>');
-$('body').append('
-//<![CDATA[
-<script type="text/javascript">
-    require(['converse'], function (converse) {
-        converse.initialize({
-            allow_otr: true,
-            auto_list_rooms: false,
-            auto_subscribe: false,
-            bosh_service_url: 'https://bind.opkode.im', // Please use this connection manager only for testing purposes
-            debug: true ,
-            hide_muc_server: false,
-            i18n: locales['en'], // Refer to ./locale/locales.js to see which locales are supported
-            prebind: false,
-            show_controlbox_by_default: true,
-            xhr_user_search: false,
-        });
-    });   
-</script>
-//]]>
+$('body').append('\
+<script type="text/javascript"> \
+    require(['converse'], function (converse) { \
+        converse.initialize({ \
+            allow_otr: true, \
+            auto_list_rooms: false, \
+            auto_subscribe: false, \
+            bosh_service_url: 'https://bind.opkode.im', // Please use this connection manager only for testing purposes \
+            debug: true , \
+            hide_muc_server: false, \
+            i18n: locales['en'], // Refer to ./locale/locales.js to see which locales are supported \
+            prebind: false, \
+            show_controlbox_by_default: true, \
+            xhr_user_search: false, \
+        }); \
+    }); \
+</script> \
 '); 
